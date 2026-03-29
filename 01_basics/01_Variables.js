2feaef8263e89ded1c515a837155187d5174c5af
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-03-29 17:08:29 +0530

    learned Variable declaration

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 471926;
+let accountEmail = "Pravin@google.com";
+var accountPassword = "12345";
+accountCity = "Mumbai";
+let accountState;
+
+// accountId = 2; ...............cannot change const variable
+
+console.log(accountId);
+
+console.log([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+Prefer not to use var
+-because of issue in block scope and functional scope
+*/
