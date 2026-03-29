255565ba0d2ac28a6b4c07d35c3d5dd7dd2c1cf7
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-03-29 17:29:46 +0530

    learned dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all JS code as a newer version
+
+//alert( 3 + 3)  //we are using node.js ,not browser
+
+console.log(2 + 3);
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined => value is not assigned
+//symbol => to find uniqueness
+
+//object
+
+
+console.log(typeof "Pravin");
+console.log(typeof null);       //null is an object
+console.log(typeof undefined); //undefined
+
