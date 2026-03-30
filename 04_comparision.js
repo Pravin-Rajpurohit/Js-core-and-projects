be88207c499b366666d40a204fe76e0eb1212cc5
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-03-30 19:34:50 +0530

    performed comparision operations

---
+++ 04_comparision.js
@@ -0,0 +1,14 @@
+console.log('2' > 1);
+console.log('02' > 1);
+
+console.log(null > 0);      //null is converted to -> 0
+console.log(null == 0);    //***null is undefined in ==
+console.log(null >= 0);    //null is converted to -> 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+
+console.log("2" == 2);
+console.log("2" === 2);
+
