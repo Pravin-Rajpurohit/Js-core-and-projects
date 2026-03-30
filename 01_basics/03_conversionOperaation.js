0ae1304e682518240ef43e8c5f0d442cc6aae133
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-03-30 19:20:45 +0530

    Performed conversion operations

--- 01_basics/03_conversionOperaation.js
+++ 01_basics/03_conversionOperaation.js
@@ -23,6 +23,47 @@
 //"" => false
 //"hitesh" => true
 
-let num =33
-let stringNumber = String(num)
-console.log(`${stringNumber} type: ${typeof(stringNumber)}`);
+// let num =33
+// let stringNumber = String(num)
+// console.log(`${stringNumber} type: ${typeof(stringNumber)}`);
+
+
+// ********************  OPERATIONS ***********************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 4);
+// console.log(2 - 4);
+// console.log(2 * 4);
+// console.log(2 ** 4);
+// console.log(2 / 4);
+// console.log(2 % 4);
+
+// console.log('1' + 2);
+// console.log('1' + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(typeof +true);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
+
+
