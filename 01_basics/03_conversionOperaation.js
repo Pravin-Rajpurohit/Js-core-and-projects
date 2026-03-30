671325212b8a52b760cf82bcf63c64e60ce11217
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-03-30 19:02:21 +0530

    Performed Conversion Operations

---
+++ 01_basics/03_conversionOperaation.js
@@ -0,0 +1,28 @@
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// //"33" => 33
+//"33abc" => NaN
+//true  => 1; false => 0
+//null => 0
+//undefined => NaN
+
+
+// let isLoggedIn = "1"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+//"" => false
+//"hitesh" => true
+
+let num =33
+let stringNumber = String(num)
+console.log(`${stringNumber} type: ${typeof(stringNumber)}`);
