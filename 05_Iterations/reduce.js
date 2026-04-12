bc9c44e00a9b3337c4fb340a492653b439a1312e
Pravin Rajpurohit <pravinrajpurohit479@gmail.com>
2026-04-13 00:13:56 +0530

    Update reduce.js

--- 05_Iterations/reduce.js
+++ 05_Iterations/reduce.js
@@ -1,3 +1,26 @@
 const myNums = [1,2,3]
 
-const myTotal = myNums.reduce()
+const myTotal = myNums.reduce( (acc , curr) => acc + curr ,0) 
+// console.log(myTotal);
+
+const shopingCart = [
+    {
+        item: "Course 1",
+        price: 999
+    },
+    {
+        item: "Course 2",
+        price: 666
+    },
+    {
+        item: "Course 3",
+        price: 777
+    },
+    {
+        item: "Course 4",
+        price: 111
+    }
+]
+
+const totalCartValue = shopingCart.reduce( (acc , item) => acc + item.price ,0)
+console.log(totalCartValue);x
